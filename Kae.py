0feb1b34042b8fca82f6f63f95ae678e1d28ac9f
kaetssu <68799123+kaetssu@users.noreply.github.com>
2024-02-02 20:05:18 -0600

    Kae.py

--- Kae.py
+++ Kae.py
@@ -2,25 +2,6 @@ import streamlit as st
 
 import pandas as pd 
 
-import numpy as np 
-
-st.title('Titulo') 
-
-PETER MONTALVO GARCIA7:43 PM
-main.py
-
-PETER MONTALVO GARCIA7:47 PM
-n = st.slider("n", 5,100, step=1)
-
-chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
-
-st.line_chart(chart_data)
-
-PETER MONTALVO GARCIA7:49 PM
-import streamlit as st 
-
-import pandas as pd 
-
 import numpy as np
 
 
